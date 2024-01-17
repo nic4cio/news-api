aeb041b0611588f97c744fc2d81b0915c7046d2c
Gabriel Nicácio <gabrielnn_1@live.com>
2024-01-17 18:35:39 -0300

    logs

--- src/main/java/tech/devinhouse/newsapi/service/UsuarioService.java
+++ src/main/java/tech/devinhouse/newsapi/service/UsuarioService.java
@@ -1,5 +1,7 @@
 package tech.devinhouse.newsapi.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tech.devinhouse.newsapi.model.Usuario;
@@ -11,22 +13,28 @@ import java.util.Optional;
 @Service
 public class UsuarioService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UsuarioService.class);
+
     @Autowired
     private UsuarioRepository usuarioRepository;
 
     public List<Usuario> getAllUsuarios() {
+        LOGGER.debug("Getting all usuarios");
         return usuarioRepository.findAll();
     }
 
     public Optional<Usuario> getUsuarioById(Long id) {
+        LOGGER.debug("Getting usuario by id: {}", id);
         return usuarioRepository.findById(id);
     }
 
     public Usuario saveUsuario(Usuario usuario) {
+        LOGGER.info("Saving usuario: {}", usuario);
         return usuarioRepository.save(usuario);
     }
 
     public void deleteUsuario(Long id) {
+        LOGGER.warn("Deleting usuario with id: {}", id);
         usuarioRepository.deleteById(id);
     }
 }
